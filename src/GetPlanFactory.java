d880fb592b0d725cda659f04fb2004555a1a5765
worapakorn <pupupuwora@gmail.com>
2023-08-28 16:25:32 +0700

    Initial commit
    Initial commit

--- src/GetPlanFactory.java
+++ src/GetPlanFactory.java
@@ -2,13 +2,13 @@ public class GetPlanFactory {
 
     //use getPlan method to get object of type Plan
     public Plan getPlan(String planType){
-        if(planType.equalsIgnoreCase("DOMESTICPLAN")) {
+        if(planType.equalsIgnoreCase("DOMESTICPLAN") || planType.equalsIgnoreCase("dp")) {
             return new DomesticPlan();
         } 
-        else if(planType.equalsIgnoreCase("COMMERCIALPLAN")){
+        else if(planType.equalsIgnoreCase("COMMERCIALPLAN") || planType.equalsIgnoreCase("cp")){
             return new CommercialPlan();
         } 
-        else if(planType.equalsIgnoreCase("INSTITUTIONALPLAN")) {
+        else if(planType.equalsIgnoreCase("INSTITUTIONALPLAN") || planType.equalsIgnoreCase("ip")) {
             return new InstitutionalPlan();
         }
         return null;
