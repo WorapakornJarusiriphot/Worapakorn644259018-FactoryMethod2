32b3c82f8a92c59ab9b2d937300dbf5a47da1755
worapakorn <pupupuwora@gmail.com>
2023-08-28 16:22:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Worapakorn644259018-FactoryMethod2
+ ชื่อ นายวรปกร จารุศิริพจน์ รหัสนักศึกษา644259018 หมู่เรียน64/45     FactoryMethod2 (Link GitHub)
---
+++ src/DomesticPlan.java
@@ -0,0 +1,6 @@
+public class DomesticPlan extends Plan{
+    @Override
+    public void getRate(){
+        rate=3.50;
+    }
+}//end of DomesticPlan class.
